2134dc898c65d2c66c322281d25507682a0a2075
Jordi Prats <jprats@systemadmin.es>
2017-05-25 13:21:15 +0200

    neteja service

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,12 @@
 class aide(
-                            $manage_package        = true,
-                            $package_ensure        = 'installed',
-                            $manage_service        = true,
-                            $manage_docker_service = true,
-                            $service_ensure        = 'running',
-                            $service_enable        = true,
-                          ) inherits aide::params{
+            $manage_package = true,
+            $package_ensure = 'installed',
+          ) inherits aide::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
 
   class { '::aide::install': }
   -> class { '::aide::config': }
-  ~> class { '::aide::service': }
   -> Class['::aide']
 
 }
